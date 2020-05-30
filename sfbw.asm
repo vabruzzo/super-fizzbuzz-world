8a328947c8ec6ff28d7222f883565fff79172379
Vincent Abruzzo <vgabruzzo@gmail.com>
2020-05-30 14:47:14 -0400

    edit comments

--- sfbw.asm
+++ sfbw.asm
@@ -10,14 +10,12 @@
 ;   else
 ;     set power-up status to small
 ;
-; $7E0019 - player power-up status:
-;             #$00 = small
-;             #$01 = big
-;             #$02 = cape
-;             #$03 = fire
-;           absolute address: $19
-; $7E0DBF - current player coin count
-;           direct page address: $0BDF
+; $0019 - power-up status address:
+;           #$00 = small
+;           #$01 = big
+;           #$02 = cape
+;           #$03 = fire
+; $0DBF - coin count address
 
 ORG $008F25             ; the SMW RAM address to insert our code
 autoclean JSL FizzBuzz  ; jump to FizzBuzz
@@ -32,28 +30,29 @@ LDA $0DBF               ; load coin count into A
 JSR Mod15               ; jump to Mod15
 BNE TestMod5            ; if A (coin count) mod 15 != 0 branch to TestMod5
 LDX #$03                ; load value 3 into X
-STX $19                 ; store the value of X (3) into power-up status address to set fire
+STX $0019                 ; store the value of X (3) into power-up status address to set fire
 BRA Return              ; branch to Return
 TestMod5:
 LDA $0DBF               ; load coin count back into A since Mod15 overwrote it
 JSR Mod5                ; jump to Mod5
 BNE TestMod3            ; if A (coin count) mod 5 != 0 branch to TestMod3
 LDX #$02                ; load value 2 into X
-STX $19                 ; store the value of X (2) into power-up status address to set cape
+STX $0019                 ; store the value of X (2) into power-up status address to set cape
 BRA Return              ; branch to Return
 TestMod3:
 LDA $0DBF               ; load coin count back into A since Mod5 overwrote it
 JSR Mod3                ; jump to Mod3
 BNE SetSmall            ; if A (coin count) mod 3 != 0 branch to SetSmall
 LDX #$01                ; load value 1 into X
-STX $19                 ; store the value of X (1) into power-up status address to set big
+STX $0019                 ; store the value of X (1) into power-up status address to set big
 BRA Return              ; branch to Return
 SetSmall:
-STZ $19                 ; store the value 0 into power-up status address to set small
+STZ $0019                 ; store the value 0 into power-up status address to set small
 Return:
 LDA $0DBF               ; load the coin count back into A, as it was when we inserted our code
 RTL                     ; return to where we inserted our initial Jump to FizzBuzz
 
+; FIXME refactor these 3 mod subroutines into 1 if possible
 Mod15:                  ; loops until A - 15 < 0 and carry flag is cleared
 SEC                     ; set the carry flag
 SBC #$0F                ; subtract 15 from A, initially the coin count
