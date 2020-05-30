51932d85a3c23f97b833df590d85f31b3f6343ea
Vincent Abruzzo <vgabruzzo@gmail.com>
2020-05-30 11:27:30 -0400

    refactor

--- sfbw.asm
+++ sfbw.asm
@@ -1,57 +1,75 @@
-; Super FizzBuzz World
-
-; $7E0019 - player powerup status, #$00 = small, #$01 = big, #$02 = cape, #$03 = fire
+; Super FizzBuzz World, a Super Mario World ROM patch
+;
+; on each coin count increase
+;   if coin count is divisible by 3 and 5
+;     set power-up status to fire
+;   else if coin count is divisible by 5
+;     set power-up status to cape
+;   else if coin count is divisible by 3
+;     set power-up status to big
+;   else
+;     set power-up status to small
+;
+; $7E0019 - player power-up status:
+;             #$00 = small
+;             #$01 = big
+;             #$02 = cape
+;             #$03 = fire
+;           absolute address: $19
 ; $7E0DBF - current player coin count
+;           direct address: $0BDF
 
-ORG $008F25               ; SWM RAM address to insert our code (increase coins by 1 subroutine)
-autoclean JSL FizzBuzz    ; jump to custom code
-NOP                       ; no-ops to take up some empty space caused by our hijack, i.e.,
-NOP                       ; the code we hijack is 6 bytes, the JSL we inject is 4, so we add 2 bytes
+ORG $008F25             ; the SMW RAM address to insert our code
+autoclean JSL FizzBuzz  ; jump to FizzBuzz
+NOP                     ; the JSL we insert is 4 bytes long but the code we overwrite is 6
+NOP                     ; add 2 1 byte no-ops to fill that empty space
 
-freecode                  ; tells assembler to place remaining code in free space in ROM
+freecode                ; tells assembler to place remaining code in free space in ROM
 
 FizzBuzz:
-INC $0DBF                 ; we overwrote this code so we do it here, it adds 1 to coin count
-LDA $7E0DBF               ; load coin count into A register
-LDX #$00                  ; load 0 into X register
-STX $19                   ; store value of X register (0) to powerup status to default to small
-JSR Modulus3              ; jump to Modulus3 subroutine down below
-BNE CheckModulus5         ; if coin count mod 3 was not 0, skip to next check, otherwise set to big
-LDX #$01                  ; load 01 into X register
-STX $19                   ; store the value of X register (01) into powerup status to set big
-CheckModulus5:
-LDA $7E0DBF               ; load coin count back into A since Modulus3 subroutine overwrote it
-JSR Modulus5              ; jump to Modulus5
-BNE CheckModulus15        ; if coin count mod 5 was not 0, skip to next check, otherwise set to cape
-LDX #$02                  ; load 02 into X register
-STX $19                   ; store the value of X register (02) into powerup status to set cape
-CheckModulus15:
-LDA $7E0DBF               ; load coin count back into A
-JSR Modulus15             ; jump to Modulus15
-BNE Return                ; if coin count mod 15 was not 0, skip to return, otherwise set to fire
-LDX #$03                  ; load 03 into X register
-STX $19                   ; store the value of X register (03) into powerup status to set fire
+INC $0DBF               ; we overwrote this instruction during our insertion so we run it here
+LDA $0DBF               ; load coin count into A
+JSR Mod15               ; jump to Mod15
+BNE CheckMod5           ; if A (coin count) mod 15 != 0 branch to Mod5
+LDX #$03                ; load value 3 into X
+STX $19                 ; store the value of X (3) into power-up status address to set fire
+JSR Return              ; jump to Return
+CheckMod5:
+LDA $0DBF               ; load coin count back into A since Mod15 overwrote it
+JSR Mod5                ; jump to Mod5
+BNE CheckMod3           ; if A (coin count) mod 5 != 0 branch to Mod3
+LDX #$02                ; load value 2 into X
+STX $19                 ; store the value of X (2) into power-up status address to set cape
+JSR Return              ; jump to Return
+CheckMod3:
+LDA $0DBF               ; load coin count back into A since Mod5 overwrote it
+JSR Mod3                ; jump to Mod3
+BNE Return              ; if A (coin count) mod 3 != 0 branch to Return
+LDX #$01                ; load value 1 into X
+STX $19                 ; store the value of X (1) into power-up status address to set big
+JSR Return              ; jump to Return
+STZ $19                 ; store the value 0 into power-up status address to set small
 Return:
-LDA $7E0DBF               ; load the coin count back into A so the code we hijacked can continue
-RTL                       ; return to where we hijacked the code
+LDA $0DBF               ; load the coin count back into A, as it was when we inserted our code
+RTL                     ; return to where we inserted our code
 
-Modulus3:
-SEC                       ; set the carry flag
-SBC #$03                  ; subtract 3 from the A register, i.e. the coin count
-BCS Modulus3              ; if carry flag is set, go back to Modulus3, otherwise proceed
-ADC #$03                  ; add 3 back to A
-RTS                       ; return to where we left off above
+Mod15:                  ; loops until A - 15 < 0 and carry flag is cleared
+SEC                     ; set the carry flag
+SBC #$0E                ; subtract 15 from A, initially the coin count
+BCS Mod15               ; if carry flag is set, go back to Mod15, otherwise proceed
+ADC #$0E                ; add 15 back to A
+RTS                     ; return to where we jumped from
 
-Modulus5:
+Mod5:
 SEC
 SBC #$05
-BCS Modulus5
+BCS Mod5
 ADC #$05
 RTS
 
-Modulus15:
+Mod3:
 SEC
-SBC #$0F
-BCS Modulus15
-ADC #$0F
+SBC #$03
+BCS Mod3
+ADC #$03
 RTS
