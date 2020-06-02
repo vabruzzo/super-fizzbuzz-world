e3b5469d536c3fe0909a01bfdcdcb6a8598d9212
vince abruzzo <vgabruzzo@gmail.com>
2020-06-02 01:41:38 -0400

    refactor modulo

--- sfbw.asm
+++ sfbw.asm
@@ -31,21 +31,28 @@ freecode                ; tell assembler to place remaining code in free space i
 FizzBuzz:
 INC !CoinCount          ; add 1 to coin count, we overwrote this instruction during our insertion so we run it here
 LDA !CoinCount          ; load coin count into A
-JSR Mod15               ; jump to Mod15, compute A mod 15
+PHX                     ; push X onto stack so we can retrieve it later
+LDX #$0F                ; load 15 into X
+STX $00                 ; store X in scratch RAM
+JSR Mod                 ; jump to Mod15, compute A mod 15
 BNE TestMod5            ; if last operation != 0 branch to TestMod5
 LDA #$03                ; load value 3 into A
 STA !PowerUpStatus      ; store the value of A into power-up status address
 BRA Return              ; branch to Return
 TestMod5:
 LDA !CoinCount          ; load coin count back into A
-JSR Mod5                ; jump to Mod5, compute A mod 5
+LDX #$05                ; load 5 into X
+STX $00                 ; store X in scratch RAM
+JSR Mod                 ; jump to Mod5, compute A mod 5
 BNE TestMod3            ; if last operation != 0 branch to TestMod3
 LDA #$02                ; load value 2 into A
 STA !PowerUpStatus      ; store the value of A into power-up status address
 BRA Return              ; branch to Return
 TestMod3:
 LDA !CoinCount          ; load coin count back into A
-JSR Mod3                ; jump to Mod3, compute A mod 3
+LDX #$03                ; load 3 into X
+STX $00                 ; store X in scratch RAM
+JSR Mod                 ; jump to Mod3, compute A mod 3
 BNE SetSmall            ; if last operation != 0 branch to SetSmall
 LDA #$01                ; load value 1 into A
 STA !PowerUpStatus      ; store the value of A into power-up status address
@@ -53,27 +60,13 @@ BRA Return              ; branch to Return
 SetSmall:
 STZ !PowerUpStatus      ; store the value 0 into power-up status address
 Return:
+PLX                     ; pull X from stack and load into X
 LDA !CoinCount          ; load coin count back into A, as it was when we inserted our code
 RTL                     ; return to where we inserted our initial jump to FizzBuzz
 
-; FIXME refactor these 3 mod subroutines into 1 if possible
-Mod15:                  ; loops until A - 15 < 0 and carry flag is cleared
+Mod:                    ; loops until A - X < 0 and carry flag is cleared
 SEC                     ; set the carry flag
-SBC #$0F                ; subtract 15 from A, initially the coin count
-BCS Mod15               ; if carry flag is set, go back to Mod15, otherwise proceed
-ADC #$0F                ; add 15 back to A
+SBC $00                 ; subtract X from A, initially the coin count
+BCS Mod                 ; if carry flag is set, go back to Mod15, otherwise proceed
+ADC $00                 ; add X back to A
 RTS                     ; return to where we jumped from
-
-Mod5:
-SEC
-SBC #$05
-BCS Mod5
-ADC #$05
-RTS
-
-Mod3:
-SEC
-SBC #$03
-BCS Mod3
-ADC #$03
-RTS
