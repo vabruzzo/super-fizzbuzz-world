888432ea7b21f14f98eae48b71566629871af161
vince abruzzo <vgabruzzo@gmail.com>
2020-06-02 10:45:38 -0400

    rm x

--- sfbw.asm
+++ sfbw.asm
@@ -14,10 +14,10 @@
 ; power-up status address:
 !PowerUpStatus = $0019
 ;   possible values:
-;     #$00 = small
-;     #$01 = big
-;     #$02 = cape
-;     #$03 = fire
+;     0 = small
+;     1 = big
+;     2 = cape
+;     3 = fire
 ; coin count address:
 !CoinCount = $0DBF
 
@@ -30,43 +30,41 @@ freecode                ; tell assembler to place remaining code in free space i
 
 FizzBuzz:
 INC !CoinCount          ; add 1 to coin count, we overwrote this instruction during our insertion so we run it here
+LDA #$0F                ; load 15 into A
+STA $00                 ; store A in scratch RAM
 LDA !CoinCount          ; load coin count into A
-PHX                     ; push X onto stack so we can retrieve it later
-LDX #$0F                ; load 15 into X
-STX $00                 ; store X in scratch RAM
-JSR Mod                 ; jump to Mod, compute A mod X
+JSR Mod                 ; jump to Mod, compute A mod value at $00 (15)
 BNE TestMod5            ; if last operation != 0 branch to TestMod5
-LDA #$03                ; load value 3 into A
-STA !PowerUpStatus      ; store the value of A into power-up status address
+LDA #$03                ; load 3 into A
+STA !PowerUpStatus      ; store A in power-up status address
 BRA Return              ; branch to Return
 TestMod5:
+LDA #$05                ; load 5 into A
+STA $00                 ; store A in scratch RAM
 LDA !CoinCount          ; load coin count back into A
-LDX #$05                ; load 5 into X
-STX $00                 ; store X in scratch RAM
-JSR Mod                 ; jump to Mod, compute A mod X
+JSR Mod                 ; jump to Mod, compute A mod value at $00 (5)
 BNE TestMod3            ; if last operation != 0 branch to TestMod3
-LDA #$02                ; load value 2 into A
-STA !PowerUpStatus      ; store the value of A into power-up status address
+LDA #$02                ; load 2 into A
+STA !PowerUpStatus      ; store A in power-up status address
 BRA Return              ; branch to Return
 TestMod3:
+LDA #$03                ; load 3 into A
+STA $00                 ; store A in scratch RAM
 LDA !CoinCount          ; load coin count back into A
-LDX #$03                ; load 3 into X
-STX $00                 ; store X in scratch RAM
-JSR Mod                 ; jump to Mod, compute A mod X
+JSR Mod                 ; jump to Mod, compute A mod value at $00 (3)
 BNE SetSmall            ; if last operation != 0 branch to SetSmall
-LDA #$01                ; load value 1 into A
-STA !PowerUpStatus      ; store the value of A into power-up status address
+LDA #$01                ; load 1 into A
+STA !PowerUpStatus      ; store A in power-up status address
 BRA Return              ; branch to Return
 SetSmall:
-STZ !PowerUpStatus      ; store the value 0 into power-up status address
+STZ !PowerUpStatus      ; store 0 in power-up status address
 Return:
-PLX                     ; pull X from stack and load into X
 LDA !CoinCount          ; load coin count back into A, as it was when we inserted our code
 RTL                     ; return to where we inserted our initial jump to FizzBuzz
 
-Mod:                    ; loops until A - X < 0 and carry flag is cleared
+Mod:                    ; loops until A - value at $00 < 0 and carry flag is cleared
 SEC                     ; set the carry flag
-SBC $00                 ; subtract X from A, initially the coin count
-BCS Mod                 ; if carry flag is set, go back to Mod15, otherwise proceed
-ADC $00                 ; add X back to A
+SBC $00                 ; subtract value at $00 from A
+BCS Mod                 ; if carry flag is set, go back to Mod, otherwise proceed
+ADC $00                 ; add value at $00 back to A
 RTS                     ; return to where we jumped from
